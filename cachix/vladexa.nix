a2a924a55e6f25f782ff5ec3dfbb8e5e302b465f
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-22 15:43:52 +0200

    Add personal cachix

---
+++ cachix/vladexa.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://vladexa.cachix.org"
+      ];
+      trusted-public-keys = [
+        "vladexa.cachix.org-1:ThmpOQRxh92xt3RMgUA5XOEklLhW1pLQa5lPTREozO4="
+      ];
+    };
+  };
+}
