bb0cc9dc3a3967a62f242cb066bbbd6261560946
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-24 00:19:26 +0200

    changes from workstation on čet 24 jul 2025 00:19:26 CEST

--- modules/yt-dlp.nix
+++ modules/yt-dlp.nix
@@ -1,10 +1,11 @@
 {
-  programs.yt-dlp.config = {
+  config = { 
+		programs.yt-dlp.config = {
     sponsorblock-remove = "sponsor";
     cookies-from-browser = "firefox";
     mark-watched = true;
     embed-metadata = true;
     embed-thumbnail = true;
     concurrent-fragments = 4;
-  };
+  }; }
 }
