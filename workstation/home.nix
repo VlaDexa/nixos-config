72e960912231b2dfc35c1b842142a44546d2f22c
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-25 22:14:22 +0200

    Add qbittorrent

--- workstation/home.nix
+++ workstation/home.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
   home-manager.users.vladexa = {
-    home.packages = with pkgs; [ mullvad-vpn ];
+    home.packages = with pkgs; [
+      mullvad-vpn
+      qbittorrent
+    ];
     programs.yt-dlp.enable = true;
   };
 }
