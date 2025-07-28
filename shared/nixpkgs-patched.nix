f268fd5fe3dfdf6a169390dc849420831328cf0d
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-28 14:28:31 +0200

    Linux 6.16 patch

--- shared/nixpkgs-patched.nix
+++ shared/nixpkgs-patched.nix
@@ -4,12 +4,16 @@ let
   pkgs = (import nixpkgs { inherit system; });
   nixpkgs-patched' = (
     pkgs.applyPatches {
-      name = "nixpkgs-patched-414391";
+      name = "nixpkgs-patched-414391-428988";
       src = nixpkgs;
       patches = [
         (pkgs.fetchpatch {
           url = "https://github.com/NixOS/nixpkgs/pull/414391.patch";
           sha256 = "sha256-DPy3yViEIO/e/Skr0gnMFqeFq77uOdJPWiXCSeKVxdQ=";
+        })
+        (pkgs.fetchpatch {
+          url = "https://github.com/NixOS/nixpkgs/pull/428988.patch";
+          sha256 = "sha256-huuLo6rD6Owfv1PwBjSqPjhS3UcBU2YHox8rjylSxFA=";
         })
       ];
     }
