6bdfbcd015ac715ed38dbdceffdd559d6e1bd665
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-28 17:50:25 +0200

    Bcachefs dedup test

--- nixosConfigs/shared/nixpkgs-patched.nix
+++ nixosConfigs/shared/nixpkgs-patched.nix
@@ -4,12 +4,16 @@ let
   pkgs = (import nixpkgs { inherit system; });
   nixpkgs-patched' = (
     pkgs.applyPatches {
-      name = "nixpkgs-patched-414391";
+      name = "nixpkgs-patched-414391-bcachefs_dedup";
       src = nixpkgs;
       patches = [
         (pkgs.fetchpatch {
           url = "https://github.com/NixOS/nixpkgs/pull/414391.patch";
           sha256 = "sha256-DPy3yViEIO/e/Skr0gnMFqeFq77uOdJPWiXCSeKVxdQ=";
+        })
+        (pkgs.fetchpatch {
+          url = "https://github.com/NixOS/nixpkgs/pull/429126.patch";
+          sha256 = "sha256-mO8Jda83Nt/4DoR1IQwc0RUe7mOHzo1ySwBsumrZBNo=";
         })
       ];
     }
