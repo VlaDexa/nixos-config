3e78db62657e686fc19f95102343a90e6bf823bc
Vladislav Grechannik <vgrechannik@gmail.com>
2025-09-01 09:30:39 +0200

    Add compose key

--- nixosConfigs/workstation/hyprland/settings/input.nix
+++ nixosConfigs/workstation/hyprland/settings/input.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings.input = {
     kb_layout = "us,ru";
-    kb_options = "grp:alt_shift_toggle";
+    kb_options = "grp:alt_shift_toggle,compose:ralt";
     numlock_by_default = true;
   };
 }
