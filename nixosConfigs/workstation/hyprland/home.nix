702daabf5def62f67dc38928dc74409edb148c1a
Vladislav Grechannik <vgrechannik@gmail.com>
2025-09-02 15:29:18 +0200

    Add dunst (not tested with kde :)

--- nixosConfigs/workstation/hyprland/home.nix
+++ nixosConfigs/workstation/hyprland/home.nix
@@ -18,5 +18,26 @@
     systemdTargets = [ "hyprland-session.target" ];
   };
 
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        width = 300;
+        height = "(0, 300)";
+        offset = "(30, 50)";
+        origin = "top-right";
+        transparency = 10;
+        frame_color = "#eceff1";
+        font = "Droid Sans 9";
+      };
+
+      urgency_normal = {
+        background = "#37474f";
+        foreground = "#eceff1";
+        timeout = 10;
+      };
+    };
+  };
+
   programs.wofi.enable = true;
 }
