11643383c5b2482df397fa25347fbc7932c871bd
Vladislav Grechannik <vgrechannik@gmail.com>
2025-09-26 17:31:57 +0200

    Add bcachefs out of tree module PR

--- flake.nix
+++ flake.nix
@@ -62,6 +62,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/414391.patch";
       flake = false;
     };
+    nixpkgs-patch-444428 = {
+      url = "https://github.com/NixOS/nixpkgs/pull/444428.patch";
+      flake = false;
+    };
     dolphin-overlay = {
       url = "github:rumboon/dolphin-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
