db78e4becfff9dbf96ffa80bbfaca1047684c592
Vladislav Grechannik <vgrechannik@gmail.com>
2025-10-09 08:00:40 +0200

    Add lanzaboote cache

---
+++ cachix/lanzaboote.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://lanzaboote.cachix.org"
+      ];
+      trusted-public-keys = [
+        "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
+      ];
+    };
+  };
+}
