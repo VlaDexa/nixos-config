b35e896b90b9a730230fcfcbb57b867296a49bf5
Vladislav Grechannik <vgrechannik@gmail.com>
2025-12-28 05:25:16 +0100

    still fixing up stuff from being fired :(

--- nixosConfigs/workstation/configuration.nix
+++ nixosConfigs/workstation/configuration.nix
@@ -14,36 +14,7 @@
   services.userborn.enable = true;
   system.nixos-init.enable = true; # Rust based init system or something
 
-  sops.secrets = {
-    private-key = { };
-    preshared-key = { };
-    # peertube-runner-token = {
-    #   owner = config.services.peertube-runner.user;
-    #   group = config.services.peertube-runner.group;
-    # };
-  };
   networking.hostName = "workstation";
-  networking.wg-quick.interfaces.wg0 = {
-    address = [
-      "172.30.0.8/32"
-      "2a01:4f9:c012:cb73:ac1e::8/128"
-    ];
-    dns = [ "1.1.1.1" ];
-    privateKeyFile = config.sops.secrets.private-key.path;
-    autostart = false;
-
-    peers = [
-      {
-        publicKey = "EbVAcKd4I2bUh+vggZEAvi4Mft09a08e58v7VrDYCDo=";
-        presharedKeyFile = config.sops.secrets.preshared-key.path;
-        allowedIPs = [
-          "49.13.8.220"
-        ];
-        endpoint = "officevpn.solved-hub.com:51820";
-        persistentKeepalive = 15;
-      }
-    ];
-  };
 
   networking = {
     networkmanager.enable = false;
