b3e9e73649653a0a6aa7ca7cbf1981484c83bcf9
Vladislav Grechannik <vgrechannik@gmail.com>
2026-02-20 18:09:12 +0100

    start gomuks at start

--- modules/matrix-client.nix
+++ modules/matrix-client.nix
@@ -1,6 +1,11 @@
 {
   flake.modules.homeManager.matrix-client =
-    { pkgs, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
     {
       programs.element-desktop = {
         enable = true;
@@ -10,6 +15,17 @@
           feature_group_calls = true;
           feature_element_call_video_rooms = true;
         };
+      };
+
+      systemd.user.services.gomuks-web = {
+        Install.WantedBy = [ config.wayland.systemd.target ];
+        Service = {
+          ExecStart = lib.getExe' pkgs.gomuks-web "gomuks-web";
+          Restart = "on-failure";
+          RestartSec = "5s";
+          Type = "exec";
+        };
+        Unit.Description = pkgs.gomuks-web.meta.description;
       };
 
       home.packages = [ pkgs.fluffychat ];
