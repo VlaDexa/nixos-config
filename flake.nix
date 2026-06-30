d6ae9a2c46750624c22af70473c8af24c12135d7
Vladislav Grechannik <vgrechannik@gmail.com>
2026-06-30 14:13:10 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'bore-scheduler':
        'github:firelzrd/bore-scheduler/507dca0bbc4db73f1a08ef03ea6d36e8cb1b8156?narHash=sha256-kMUr4yYvrTZr092C5nhVSOXU/Kvf0KYZBnlM0gkasZo%3D' (2026-06-27)
      → 'github:firelzrd/bore-scheduler/16bf5baebbb42cdba393c501ba9c2af5f84e4749?narHash=sha256-vSQsqXVcQMyPhQunUMRjfgeqoZDnj/uci95GPvAv/AU%3D' (2026-06-30)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/4ad9aaae70c9aaab504127f926c0fa9cfbc2b365?narHash=sha256-PHTCpYZCMzJYS3phhywqRAZphKVr2zjvlGYa%2BH20ZZ4%3D' (2026-06-28)
      → 'github:nix-community/home-manager/5d72a29fc36ac21adae6ae35568fe5ee6700850f?narHash=sha256-slFTUgDy0KTPA4LBAmC/9SngDq8GCPdX%2BZR0yQHHN1E%3D' (2026-06-29)
    • Updated input 'hyprland':
        'github:hyprwm/Hyprland/75f558a536f8003bf80af5cb1a3e91529e78cb6b?narHash=sha256-OYjcMSNogYWLbeP4nxpUVJaO72o6yIK00bDYaAuQI2Y%3D' (2026-06-28)
      → 'github:hyprwm/Hyprland/254d6bae775258b554e1d3a4a853f3bc244190d9?narHash=sha256-pMax4dvo/BbKCO2zvCT8fgziiWbfXGhRH0/5uF8pyd0%3D' (2026-06-29)
    • Updated input 'hyprland/hyprutils':
        'github:hyprwm/hyprutils/9d8bf6e810597152eef8906c670b96679af2faec?narHash=sha256-pUtCphVzH1iNUTMGdJr4%2Be/yzUXA6/DZwsn%2BcyfIVJU%3D' (2026-06-21)
      → 'github:hyprwm/hyprutils/d2b40ffe7bfcb001bbf5888bb56ff646de53e7db?narHash=sha256-mjt47Xun3jPcGrXpGpYU85lzUnqvJ2rXLbQjVI1yvLo%3D' (2026-06-28)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/e73de5be04e0eff4190a1432b946d469c794e7b4?narHash=sha256-pGvFkM8N0xEkIIXDe5YYfbEAvHrk4IxBrjB/x8OomhE%3D' (2026-06-26)
      → 'github:NixOS/nixpkgs/b5aa0fbd538984f6e3d201be0005b4463d8b09f8?narHash=sha256-oPXCU/SSUokcGaJREHibG1CBX3%2Bs/W7orDWQOZDsEeQ%3D' (2026-06-29)
    • Updated input 'nixpkgs-patch-bitwarden-26-6-electron':
        'path:./0001-bitwarden-dekstop-add-patch-that-switches-to-electro.patch'
      → 'path:./0001-bitwarden-dekstop-add-patch-that-switches-to-electro.patch'
    • Updated input 'nixvim':
        'github:nix-community/nixvim/dbf9550dba8448b03e11d58e5695d6c44a464554?narHash=sha256-kjsEECqhpPnJWqhooXp6tWh2qGQftCPAo2G1GvZtKdw%3D' (2026-06-23)
      → 'github:nix-community/nixvim/c6a99bb41fb0f37e03ca38b1d81b006e98e3bf1a?narHash=sha256-b2ODGxKD3hgrwuesLtDomp2DKF3UiOn3%2BEBTDXpcqGo%3D' (2026-06-29)
    • Updated input 'nur':
        'github:nix-community/NUR/ad01fab87df3f1addd0f78e4c8d14458ebaab6e0?narHash=sha256-4mMY48QI8Ztw%2BeZAdPrY5xfAbjttZJE546aaXHGRqeE%3D' (2026-06-29)
      → 'github:nix-community/NUR/6b65a3740d7066581d718b8cb8ea62181f000274?narHash=sha256-fciEt247KJc4OSELOPylEejxlFMVzbFriGY5AyxwQJA%3D' (2026-06-30)

--- flake.nix
+++ flake.nix
@@ -61,16 +61,12 @@
     };
     # Temporary patcher until https://github.com/NixOS/nix/issues/3920 is resolved
     nixpkgs-patcher.url = "github:gepbird/nixpkgs-patcher";
-    nixpkgs-patch-bitwarden-26-6 = {
-      url = "https://github.com/NixOS/nixpkgs/pull/535399.patch";
+    nixpkgs-patch-vesktop-pnpm-update = {
+      url = "https://github.com/NixOS/nixpkgs/pull/536660.patch";
       flake = false;
     };
     nixpkgs-patch-bitwarden-26-6-electron = {
       url = "path:./0001-bitwarden-dekstop-add-patch-that-switches-to-electro.patch";
-      flake = false;
-    };
-    nixpkgs-patch-proton-ge-11 = {
-      url = "https://github.com/NixOS/nixpkgs/pull/535693.patch";
       flake = false;
     };
     bcachefs-nixpkgs-patch-429126 = {
